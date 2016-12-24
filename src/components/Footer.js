5535395c57ec7c77295ef2c5e42e2a48aaf1628b
Benjamin W. Smith <benjaminwarfield@just-another.net>
2016-12-24 14:19:47 -0500

    Fix up footer layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,9 @@ import {h} from 'preact';  /** @jsx h */
 import {Link} from 'preact-router';
 
 export const Footer = () => (
-  <div class="content">
-    <footer class="footer">
-      <div class="container">
-        <p class="text-center"><Link href="/about">About</Link> - <Link href="/contact">Contact</Link> - &copy; {new Date().getFullYear()} Benjamin Warfield Smith</p>
-      </div>
-    </footer>
-  </div>
+  <footer class="footer">
+    <div class="container">
+      <p class="text-center"><Link href="/about">About</Link> - <Link href="/contact">Contact</Link> - &copy; {new Date().getFullYear()} Benjamin Warfield Smith</p>
+    </div>
+  </footer>
 );
